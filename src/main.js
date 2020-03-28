6d6a61f5d05625cbc2e7c65b96ac34b8e81180c5
Hkmu <jiehecd@gmail.com>
2020-03-28 21:50:57 +0800

    feat: support Service Worker

--- src/main.js
+++ src/main.js
@@ -4,5 +4,15 @@ const app = new App({
 	target: document.body,
 	props: {}
 });
+console.log('serviceWorker' in navigator);
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function(registration) {
+			console.log('ServiceWorker registration successful');
+		}).catch(function(err) {
+			console.log('ServiceWorker registration failed: ', err);
+		});
+    });
+}
 
 export default app;
---
+++ src/sw.js
@@ -0,0 +1,40 @@
+const CACHE = "site-cache";
+const CACHE_LIST = ["./global.css", "./build/bundle.css", "./build/bundle.js", "./logo.svg"];
+
+self.addEventListener("install", function(evt) {
+  evt.waitUntil(preCache());
+});
+
+self.addEventListener("fetch", function(evt) {
+  evt.respondWith(fromCache(evt.request));
+});
+
+function preCache() {
+  return caches.open(CACHE).then(function(cache) {
+    return cache.addAll(CACHE_LIST);
+  });
+}
+
+function fromCache(request) {
+  return caches.open(CACHE).then(function(cache) {
+    return cache.match(request).then(function(matching) {
+      if (matching) {
+        return matching;
+      }
+      const req = request.clone();
+
+      return fetch(req).then(function(resp) {
+        if (!resp || resp.status !== 200) {
+          return resp;
+        }
+
+        const respClone = resp.clone();
+        caches.open(CACHE).then(function(cache) {
+          cache.put(request, respClone);
+        });
+
+        return resp;
+      });
+    });
+  });
+}
